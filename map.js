999805bc692a7e743e8f9970dc7d2aeadc669f49
imaspork <tyler.strong1@gmail.com>
2021-04-28 16:12:40 -0700

    removed google key

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGzkyY05335UxV-zAgXhldt7LrPMEg164&libraries=places">
+// <script src="https://maps.googleapis.com/maps/api/js?key=<REMOVED>&libraries=places">
 let map;
 let service;
 let infowindow;
